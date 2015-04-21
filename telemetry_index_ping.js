0845a9317567b899a12edd23adce78d7d2e54e13
Roberto Agostino Vitillo <ra.vitillo@gmail.com>
2015-04-21 17:03:39 +0100

    First commit.

---
+++ telemetry_index_ping.js
@@ -0,0 +1,32 @@
+var AWS = require('aws-sdk');
+var exec = require('child_process').exec;
+var simpledb = new AWS.SimpleDB();
+
+var v2DomainPrefix = "telemetry_v2_";
+
+exports.handler = function(event, context) {
+  var srcBucket = event.Records[0].s3.bucket.name;
+  var key = event.Records[0].s3.object.key;
+
+  exec('python telemetry_schema.py telemetry_v2_schema.json ' + key, function callback(error, stdout, stderr){
+    if (error) {
+      context.fail(stderr);
+    }
+
+    var dims = JSON.parse(stdout);
+    var domain = v2DomainPrefix + dims["submission_date"].substring(0, dims["submission_date"].length - 2);
+    var params = {"Attributes": [], "DomainName": domain, "ItemName": key};
+
+    for (var prop in dims) {
+      params["Attributes"].push({"Name": prop, "Value": dims[prop]});
+    }
+
+    simpledb.putAttributes(params, function (error) {
+      if (error) {
+        context.fail(error);
+      }
+
+      context.succeed(params);
+    });
+  });
+};
